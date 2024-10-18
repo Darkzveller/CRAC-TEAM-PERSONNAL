bb638eb51e6309515fc4d365058590340489e4cf
youssef <youssef.el-kattoufi@universite-paris-saclay.fr>
2024-10-18 16:52:28 +0200

    Ajout des identifiant de mon pc pour le wifi

--- CodeBaseRoulanteOTA/lib/OTA/OTA.cpp
+++ CodeBaseRoulanteOTA/lib/OTA/OTA.cpp
@@ -1,5 +1,6 @@
 // #define MON_TELEPHONE
-#define MA_FREEBOX
+// #define MA_FREEBOX
+#define MON_PC
 long i = 0;      // Déclaration d'une variable statique qui conserve sa valeur entre les appels
 #include "OTA.h" // Inclusion de la bibliothèque pour gérer l'OTA (Over-The-Air)
 // Je laisse le routeur choisir l'adresse IP et
@@ -15,9 +16,9 @@ const char *password = "youssef13"; // Mot de passe du réseau WiFi
 const char *ssid = "Freebox-587F87";                               // SSID du réseau WiFi
 const char *password = "subcrescat-degend@-parciore@2-adducturos"; // Mot de passe du réseau WiFi
 #endif
-#ifdef Mon_PC                                                      // Nom d'hôte de la carte ESP32
-const char *ssid = "Freebox-587F87";                               // SSID du réseau WiFi
-const char *password = "subcrescat-degend@-parciore@2-adducturos"; // Mot de passe du réseau WiFi
+#ifdef MON_PC                         // Nom d'hôte de la carte ESP32
+const char *ssid = "Detective-Conan"; // SSID du réseau WiFi
+const char *password = "99xS,304";    // Mot de passe du réseau WiFi
 #endif
 
 // Fonction pour gérer les opérations OTA dans une tâche séparée
@@ -148,6 +149,7 @@ void setupOTA()
       1,            /* Priorité de la tâche */
       NULL);        /* Handle de la tâche (aucun ici) */
 }
+
 void receptionWIFI()
 {
   if (SerialWIFI.available())
@@ -159,111 +161,14 @@ void receptionWIFI()
     // Supprimer les espaces ou caractères indésirables (comme le retour à la ligne)
     input.trim(); // Enlève les espaces superflus et les retours à la ligne
 
-    // if (input.equals("stop")) // Vérifier si l'entrée est "stop"
-    // {
-    //     i = 0; // Remise à zéro de la variable
-    //     SerialWIFI.println(i);
-    //     Serial.println("Valeur mise à zéro");
-    // }
-    if (input.equals("STOP")) // Vérifier si l'entrée est "stop"
+    if (input.equals("S")) // Vérifier si l'entrée est "stop"
     {
       // i = 0; // Remise à zéro de la variable
       SerialWIFI.println("STOP BASE ROULANTE");
     }
-    if (input.equals("start")) // Vérifier si l'entrée est "start"
+    if (input.equals("M")) // Vérifier si l'entrée est "start"
     {
-      SerialWIFI.println(i++); // Incrémenter i et envoyer
-      Serial.println("Incrémenté : " + String(i));
+      SerialWIFI.println("START TEST");
     }
   }
 }
-// AU cas ou j'ai mal effectué mes commentaires
-/*#include "OTA.h"
-
-const char *name_card_elec = "BaseEsp32";
-const char *ssid = "Me voici";
-const char *password = "youssef13";
-
-void ota_handle(void *parameter)
-{
-  for (;;)
-  {
-    ArduinoOTA.handle();
-    delay(3500);
-  }
-}
-void setupOTA()
-{
-  WiFi.setHostname(name_card_elec);
-
-  WiFi.mode(WIFI_STA);
-
-  WiFi.begin(ssid, password);
-  Serial.println("Connexion au WiFi en cours...");
-
-  while (WiFi.waitForConnectResult() != WL_CONNECTED)
-  {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(1000);
-    // ESP.restart();
-  }
-  // Affiche le succès de la connexion
-  Serial.println("");
-  Serial.println("Connexion établie !");
-  // Port defaults to 3232
-  // ArduinoOTA.setPort(3232);
-
-  // Hostname defaults to esp3232-[MAC]
-  ArduinoOTA.setHostname(name_card_elec);
-
-  // No authentication by default
-  // ArduinoOTA.setPassword("admin");
-
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = admin;
-  // ArduinoOTA.setPasswordHash("admin");
-
-  ArduinoOTA
-      .onStart([]()
-               {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH)
-        type = "sketch";
-      else // U_SPIFFS
-        type = "filesystem";
-
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type); })
-      .onEnd([]()
-             { Serial.println("\nEnd"); })
-      .onProgress([](unsigned int progress, unsigned int total)
-                  { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
-      .onError([](ota_error_t error)
-               {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
-
-
-  ArduinoOTA.begin();
-  SerialWIFI.begin();
-
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.print("Wifi Hostname: ");
-  Serial.println(WiFi.getHostname());
-  Serial.print("Arduino Hostname: ");
-  Serial.print(ArduinoOTA.getHostname());Serial.println(".local");
-
-  xTaskCreate(
-      ota_handle,   /* Task function. */
-//       "OTA_HANDLE", /* String with name of task. */
-//       10000,        /* Stack size in bytes. */
-//       NULL,         /* Parameter passed as input of the task */
-//       1,            /* Priority of the task. */
-//       NULL);        /* Task handle. */
-// }
