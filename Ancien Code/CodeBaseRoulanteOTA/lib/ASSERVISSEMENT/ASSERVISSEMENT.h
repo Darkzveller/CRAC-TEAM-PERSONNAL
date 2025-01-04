4b1956df117f381574e3f4ec06c3505fed295d9c
youssef <youssef.el-kattoufi@universite-paris-saclay.fr>
2025-01-04 20:09:38 +0100

    Etant donné que matthieu n'a pas réussi avec son github, et que voila quoi j'ai essayer son idée ca n'a pas fonctionner du premiere coup je remet le code ancien avant que matthieu arrive avec son idée

---
+++ Ancien Code/CodeBaseRoulanteOTA/lib/ASSERVISSEMENT/ASSERVISSEMENT.h
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+#ifndef _ASSERVISSEMENT_H
+#define _ASSERVISSEMENT_H
+
+
+
+void asservissement_roue_folle_droite_tick(double consigne, double observation);
+void asservissement_roue_folle_gauche_tick(double consigne, double observation);
+
+double regulation_vitesse_roue_folle_droite(float cons, float Vmax_consigne);
+double regulation_vitesse_roue_folle_gauche(float cons, float Vmax_consigne);
+
+void asservissement_freinage_roue_folle_gauche(float consigne, float observation);
+
+void asservissement_freinage_roue_folle_droite(float consigne, float observation);
+bool return_flag_asser_roue();
+void lauch_flag_asser_roue(bool mode);
+#endif
---
+++ Ancien Code/CodeBaseRoulanteOTA/lib/MOUVEMENT/MOUVEMENT.h
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+#ifndef _MOUVEMENT_H
+#define _MOUVEMENT_H
+
+void rotation(int consigne, int vitesse, int sens);
+void ligne_droite(int consigne, int vitesse, int sens);
+double asservissement_angle_correction(double consigne_angle, double observation_angle);
+void x_y_theta(float coordonnee_x, float coordonnee_y, float theta_fin, int vitesse);
+
+#endif
